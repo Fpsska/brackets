bb90699f62ed728c732c162d7f660807e60de769
Fpsska <73740087+Fpsska@users.noreply.github.com>
2021-03-15 22:48:36 +0300

    feat: new solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  const bracketsMap = new Map(bracketsConfig);
+  const wrongBrackets = [];
+
+  for (let i = 0; i < str.length; i++) {
+  
+      if (str[i] === bracketsMap.get(wrongBrackets[wrongBrackets.length - 1])) {
+          wrongBrackets.pop();
+      } else {
+          wrongBrackets.push(str[i]);
+      }
+
+  }
+  return wrongBrackets.length == 0;
+};
